f6d0a8736d0800fe1f1d1a32afc25c65b70c847f
Krishna Kumar <dev@krishnaaa.com>
2024-05-09 00:40:07 +0530

    added: github redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/github",
+        destination: "https://github.com/krishna8421",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
